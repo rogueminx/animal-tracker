586f0532694d5acc3082e9bc240dc0b46cca141d
John Rykken and Maggie Summers <john@>
2018-04-16 16:28:08 -0700

    Implement add-sighting functionality

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,23 @@
+class SightingsController < ApplicationController
+  def new
+    @animal = Animal.find(params[:animal_id])
+    @sighting = @animal.sightings.new
+    render :new
+  end
+
+  def create
+    @animal = Animal.find(params[:animal_id])
+    @sighting = @animal.sightings.new(sighting_params)
+    if @sighting.save
+      redirect_to animal_path(@sighting.animal)
+    else
+      render :new
+    end
+  end
+
+private
+  def sighting_params
+    params.require(:sighting).permit(:date, :lat_lng)
+  end
+
+end
