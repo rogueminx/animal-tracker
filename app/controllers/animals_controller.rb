144ae826e94c133789a90dd9410140905b7dd28b
John Rykken and Maggie Summers <john@>
2018-04-16 15:09:56 -0700

    add update and delete functionality

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -23,6 +23,26 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def edit
+    @animal = Animal.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to animals_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirect_to animals_path    
+  end
+
   private
     def animal_params
       params.require(:animal).permit(:kind)
