a13d48a83bc902e8bd986103c76f20b7ee044a98
Anna Marie Cooper and John Rykken <john@>
2018-04-17 11:51:11 -0700

    Add Faker gem. Seed database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Animal.destroy_all
+
+50.times do |index|
+  Animal.create!(kind: Faker::Cat.breed)
+end
+
+p "Created #{Animal.count} animals"
+
+# 500.times do |index|
+#   @x = random.number
+#   @y = random.number
+#   Sighting.create!
+# end
